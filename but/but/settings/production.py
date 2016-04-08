949ed2c1f3b102da7fa30a7e48facb94d61a8b40
seungkwon <yevgnenll@gmail.com>
2016-04-08 16:00:33 +0900

    s3 + cloudfront static settings
    i will use cloudfront, this settings are for it
    django-storages-redux

--- but/but/settings/production.py
+++ but/but/settings/production.py
@@ -20,13 +20,13 @@ RAVEN_CONFIG = {
 }
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-STATICFILES_STORAGE = 'fastagram.s3storage_fastagram.S3PipelineCachedStorage'
+STATICFILES_STORAGE = 'but.storage.S3PipelineCachedStorage'
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
-AWS_S3_CUSTOM_DOMAIN = 'do0qcfch9z2l2.cloudfront.net'
+AWS_S3_CUSTOM_DOMAIN = 'dntxni030r6jm.cloudfront.net'
 AWS_S3_URL_PROTOCOL = 'https'
 
-STATIC_URL = "https://do0qcfch9z2l2.cloudfront.net/"
+STATIC_URL = "https://dntxni030r6jm.cloudfront.net/"
