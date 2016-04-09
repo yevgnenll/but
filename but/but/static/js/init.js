ce28e093939c0a713ed971bf278a3046d3cc1774
seungkwon <yevgnenll@gmail.com>
2016-04-10 07:55:31 +0900

    few bugs fix
    1. price, stock must be input number
    2. 140 intro default word is deleted
    3. header 'list' link is added
    4. list image has material effect

--- but/but/static/js/init.js
+++ but/but/static/js/init.js
@@ -1,8 +1,7 @@
-function(){
+(function(){
     $(document).ready(function(){
         $('.button-collapse').sideNav();
         $('.parallax').parallax();
-        $('.textarea_material').val('New Text');
         $('.textarea_material').trigger('autoresize');
     });
 })();
