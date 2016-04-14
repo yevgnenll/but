63211ffa5e68ed4f6f0f2317c83a472dac0186f7
seungkwon <yevgnenll@gmail.com>
2016-04-14 14:22:39 +0900

    send_sms pep8
    this module is not pep8 kept

--- but/but/utils/sms_send.py
+++ but/but/utils/sms_send.py
@@ -8,30 +8,30 @@ class SMSSend():
     def __init__(self):
         self.client_id = settings.API_CLIENT
         self.key = settings.API_KEY
-        self.api_version ="1"
+        self.api_version = "1"
         self.msg_type = "sms"
 
-        self.api_url = "http://api.openapi.io/ppurio/"+ \
-                    "{api_version}/message/{msg_type}/{client_id}/".format(
-                    api_version=self.api_version,
-                    msg_type=self.msg_type,
-                    client_id=self.client_id,
-                    )
+        self.api_url = "http://api.openapi.io/ppurio/" + \
+            "{api_version}/message/{msg_type}/{client_id}/".format(
+                        api_version=self.api_version,
+                        msg_type=self.msg_type,
+                        client_id=self.client_id,
+                        )
 
         self.header = {
             "x-waple-authorization": self.key,
         }
-         
 
     def send_sms(self, dest_num, msg):
 
         res = requests.post(
-                self.api_url,  
-                data = {
-                    'dest_phone' : dest_num,
+                self.api_url,
+                data={
+                    'dest_phone': dest_num,
                     'msg_body': msg,
-                    'send_phone' : '01095716689',
+                    'send_phone': '01095716689',
                 },
-                headers= self.header)
-        
+                headers=self.header,
+        )
+
         return res
