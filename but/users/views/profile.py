012d3c6728d6fc7a51bf2e57875d36d31c7121b0
seungkwon <yevgnenll@gmail.com>
2016-04-14 12:07:41 +0900

    profile toast popup
    when profile page access they can check themselves

--- but/users/views/profile.py
+++ but/users/views/profile.py
@@ -1,5 +1,6 @@
 from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 
 from users.models import User
 
@@ -9,3 +10,26 @@ class UserProfileView(LoginRequiredMixin, DetailView):
     model = User
     slug_field = 'username'
     template_name = 'users/profile.html'
+
+    def dispatch(self, request, *args, **kwargs):
+
+        page_slug = kwargs.get('slug')
+        page_user = User.objects.get(
+                username=page_slug,
+        )
+
+        if page_user == request.user:
+            messages.add_message(
+                    request,
+                    messages.INFO,
+                    "나의 프로필에 들어왔습니다",
+            )
+        else:
+            messages.add_message(
+                    request,
+                    messages.INFO,
+                    "{{page_user}}님의 프로필에 들어왔습니다".foramt(
+                        page_user=page_user.username,
+                        )
+                    )
+        return super(UserProfileView, self).dispatch(request, args, kwargs)
